e598fb63d9af7f0dd606026171236602a36772be
KIvan1 <kirillov20403@gmail.com>
2022-04-18 09:54:04 +0300

    commit

--- lab7/move_cursor.c
+++ lab7/move_cursor.c
@@ -7,15 +7,34 @@ void move_cursor(text txt, int line, int pos)
 {
 	int k = 1;
 	node *current = txt->begin;
-	txt->cursor->position = pos;
-	while(current)
-	{
-		if (k == line)
-		{
-			txt->cursor->line = current;
-			break;
-		}		
-		current = current->next;
-		k ++;
-	}
+    if (pos >= 0)
+    {
+        txt->cursor->position = pos;
+    }
+    else
+    {
+        txt->cursor->position = 0;
+    }
+
+    if (line > 0)
+    {
+        int l = txt->length;
+        if (line > l)
+            line = l;
+
+        while(current)
+        {
+            if (k == line)
+            {
+                txt->cursor->line = current;
+                break;
+            }
+            current = current->next;
+            k ++;
+        }
+    }
+    else
+    {
+        txt->cursor->line = current;
+    }
 }
--- tests/tests.h
+++ tests/tests.h
@@ -2,16 +2,30 @@
 #define EQTEST_H
 #include <gtest/gtest.h>
 #include <gmock/gmock-matchers.h>
+#include <iostream>
 
 extern "C"{
-//#include "text/text.h"
-//#include "text/_text.h"
-//#include "common.h"
+#include "text/text.h"
+#include "text/_text.h"
+#include "common.h"
 }
 
-TEST(noroots_TEST, suite2)
+TEST(cursor_pos, sute)
 {
-    EXPECT_EQ(1, 1);
+    text txt = create_text();
+    load(txt, "input.txt");
+    move_cursor(txt, 2, 3);
+    EXPECT_EQ(txt->cursor->position, 3);
+    int k = 1;
+    node *current = txt->begin;
+    while(current != txt->cursor->line)
+    {
+        k++;
+        current = current->next;
+    }
+    EXPECT_EQ(k, 2);
+    remove_all(txt);
+    move_cursor(txt, 2, 3);
 }
 
 #endif // EQTEST_H
